6158d3314d2a87fc97138b5ec75ef8179c74a92b
pablokan <pablokan@gmail.com>
2020-08-13 20:03:13 -0300

    .

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/flaskmysql'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
